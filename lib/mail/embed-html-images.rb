45d904407ef5fbefd9c58dddd50d0458bcc54294
Phil Stewart <p.stewart@imperial.ac.uk>
2016-11-14 11:46:18 +0000

    Do not attempt to attach a downloaded image if download was not successful, Bump version to 1.0.1

--- lib/mail/embed-html-images.rb
+++ lib/mail/embed-html-images.rb
@@ -22,22 +22,33 @@ module Mail
         img_src_original = $3
         img_src_hash = Digest::SHA1.hexdigest img_src_original
 
-        # Check to see whether or not we already have this image: if not, attach it
+        # Check to see whether or not we already have this image: if not, attempt to attach it
         if !@_embedded_images[img_src_hash]
           response = Net::HTTP.get_response(URI(img_src_original))
-          attachment_filename = img_src_hash + '.' + MIME_TO_EXTENSION[response["content-type"]]
-          add_file filename: attachment_filename, content: response.body
-          @_embedded_images[img_src_hash] = attachment_filename
+
+          # If we were successful retrieving the image, attach it
+          if response.kind_of? Net::HTTPSuccess
+            attachment_filename = img_src_hash + '.' + MIME_TO_EXTENSION[response["content-type"]]
+            add_file filename: attachment_filename, content: response.body
+            @_embedded_images[img_src_hash] = attachment_filename
+          end
         end
-        current_images[img_src_hash] = @_embedded_images[img_src_hash]
 
-        attached_image = attachments[@_embedded_images[img_src_hash]]
+        # If we already have this image or have successfully attached it, update the matched img element
+        if @_embedded_images[img_src_hash]
+          current_images[img_src_hash] = @_embedded_images[img_src_hash]
 
-        # Create new src using content id of attachment
-        img_src_new = "cid:#{attached_image.cid}"
+          attached_image = attachments[@_embedded_images[img_src_hash]]
 
-        # Return substitute with new src
-        "<img #{$1}src=\"#{img_src_new}\"#{$4}>"
+          # Create new src using content id of attachment
+          img_src_new = "cid:#{attached_image.cid}"
+
+          # Return substitute with new src
+          "<img #{$1}src=\"#{img_src_new}\"#{$4}>"
+        else
+          # Attachment couldn't be attached, leave img element unchanged
+          match_string
+        end
       end
 
       # Detach any images that are no longer needed
